36229874ccf6ed7823158bdfbadef866ba9b66b8
backbook <76886049@qq.com>
2021-09-11 15:21:03 +0800

    add base

---
+++ var.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+//
+// Created by backbook on 2021/9/11.
+//
+
+#define pi 3.14159
+
+
+int main(){
+    //变量类型 变量名 = 变量初始值
+    int a = 10;
+
+    const int b = 1;
+
+    std::cout << "a = " << a << std::endl;
+
+    std::cout<< "pi = "<< pi << std::endl;
+
+    std::cout << "b = " << b << std::endl;
+
+    system("pause");
+
+
+    return 0;
+}
