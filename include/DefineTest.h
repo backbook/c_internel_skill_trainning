e94f4486e3750f4031b180350aa4adb2feaa8edd
yannsu <76886049@qq.com>
2021-09-15 16:36:14 +0800

    add DefineTest.cpp

---
+++ include/DefineTest.h
@@ -0,0 +1,11 @@
+//
+// Created by 苏锦阳 on 2021/9/15.
+//
+#include <stdio.h>
+
+
+
+#define TEST(name)\
+  do{\
+      std::cout << #name << std::endl;\
+  }while(0)
---
+++ src/DefineTest.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "../include/DefineTest.h"
+//
+// Created by 苏锦阳 on 2021/9/15.
+//
+using namespace std;
+
+
+int main(){
+
+
+
+    TEST(100);
+
+
+
+
+    return 0;
+}
